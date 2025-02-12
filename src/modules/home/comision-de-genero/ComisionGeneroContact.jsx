96e587023de4cfcb7e9b77151d6874743f9d2ff8
wulf <czh1983@gmail.com>
2025-02-12 14:26:20 -0300

    fix: se mejoró el responsivo general y el link hacia facebook

--- src/modules/home/comision-de-genero/ComisionGeneroContact.jsx
+++ src/modules/home/comision-de-genero/ComisionGeneroContact.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Typography, Box, Container } from '@mui/material';
+import { Typography, Box, Container, Link } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { SCOPES } from 'helpers/constants/i18n.ts';
 
@@ -8,6 +8,13 @@ const item = {
   flexDirection: 'column',
   alignItems: 'left',
   px: 5
+};
+
+const itemFacebook = {
+  display: 'flex',
+  flexDirection: 'column',
+  alignItems: 'flex-start',
+  wordBreak: 'break-word'
 };
 
 function ComisionGeneroContact() {
@@ -23,17 +30,21 @@ function ComisionGeneroContact() {
             {' '}
             {t('subtitle_1')}{' '}
           </Typography>
-          <Typography>
-            {' '}
-            {t('body_1')}{' '}
-            <a
-              href="https://www.facebook.com/Comisiondegenerotango/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Link
-            </a>
-          </Typography>
+          <Box
+            sx={itemFacebook}
+          >
+            <Typography>
+              {t('body_1')}
+              <Link
+                href="https://www.facebook.com/Comisiondegenerotango/"
+                target="_blank"
+                rel="noreferrer"
+                sx={{ wordBreak: 'break-word' }}
+              >
+                Link
+              </Link>
+            </Typography>
+          </Box>
           <ul>
             {t('body_2')
               .split('\n')
